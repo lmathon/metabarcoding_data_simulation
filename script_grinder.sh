cd7603c07bdd2590969c99f1f2ea9b55214ad5be
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-08-30 08:48:06 +0200

    parallel task of grinder

--- script_grinder.sh
+++ script_grinder.sh
@@ -6,10 +6,16 @@ NB_SAMPLE=30
 #### number of replicats
 NB_REPLICAT=12
 
+## run NB_SAMPLE*NB_REPLICAT simulations with grinder
 for ID_SAMPLE in `seq -w 1 $NB_SAMPLE`; do
 	((i=i%CORES)); ((i++==0)) && wait
 	##  grinder command to run with input files into folder 'Inputs'
-	#bash grinder_simulations/simulation_grinder.sh "${ID_SAMPLE}" &
+	bash grinder_simulations/simulation_grinder.sh "${ID_SAMPLE}" &
+done
+
+## deinterleave grinder fastq
+for ID_SAMPLE in `seq -w 1 $NB_SAMPLE`; do
+	((i=i%CORES)); ((i++==0)) && wait
 	for ID_REPLICAT in `seq -w 1 $NB_REPLICAT`; do
 		bash grinder_simulations/deinterleave_fastq.sh < grinder_simulations/Outputs/sample"${ID_SAMPLE}"/sample"${ID_SAMPLE}"-"${ID_REPLICAT}"-reads.fastq \
 		grinder_simulations/Outputs/grinder_teleo1/paired_end_R1/sample"${ID_SAMPLE}"-"${ID_REPLICAT}"_R1.fastq.gz \
